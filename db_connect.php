1e68c83cf5831ce6131f14d557f991b7f2a8eaa5
Madeline Ricateau <madeline.ricateau@outlook.fr>
2024-10-10 12:05:23 +0200

    Connexion à la base de données

---
+++ db_connect.php
@@ -0,0 +1,12 @@
+<?php
+try {
+    $db_connect = new PDO("mysql:host=localhost;dbname:share_my_links", 
+                          "root", 
+                          "V3nDta!");
+                          
+    $db_connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // echo "Connexion réussie ! ";  
+} catch (PDOException $e) {
+    echo "Connexion échouée : " . $e->getMessage();    
+}
