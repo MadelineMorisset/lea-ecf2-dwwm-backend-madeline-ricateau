cf7ea63dce2860f36e45df38b0484f059dd9cb6c
Madeline Ricateau <madeline.ricateau@outlook.fr>
2024-10-12 19:18:04 +0200

    Mise en place fichier de mise à jour de données

--- modify_link.php
+++ modify_link.php
@@ -1,34 +1,68 @@
 <?php
 require_once('db_connect.php');
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $id_link=$_POST['id_link'];
+$id_link= '';
+$affichage_titre_lien= '';
+// Variable pour stocker le message de $execution_request
+$execution_request_message= ''; 
 
-    $title_link_requete = $db_connect->prepare("SELECT titre_link FROM link WHERE id_link = ?");
+// changement de méthode POST pour GET afin de récupérer l'id du link à partir de l'url
+if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET["id_link"])) {
+    $id_link=$_GET['id_link'];
+    
+    $title_link_requete = $db_connect->prepare("SELECT * 
+                                                FROM link 
+                                                WHERE id_link = ?");
     $title_link_requete->execute([$id_link]);
-    $affichage_titre_lien = $title_link_requete->fetch();
+    $affichage_titre_lien = $title_link_requete->fetch(PDO::FETCH_ASSOC);
+
+    // Débogage :
+    // var_dump($affichage_titre_lien);
+    // if ($affichage_titre_lien) {
+    //     echo "Link récupéré avec succès ";
+    // } else {
+    //     echo "Erreur : le lien n'a pas été trouvé.";
+    // }
 }
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $url=$_POST['url'] ?? '';
-    $title=$_POST['title'] ?? '';
-    $comment=$_POST['comment'] ?? '';
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["id_link"])) {
+    $id_link=$_POST['id_link'];
+    $url=$_POST['url'];
+    $title=$_POST['title'];
+    $description=$_POST['description'];
 
-    if (empty($url)) {
-        $error = "Veuillez insérer une URL.";
-    }elseif (empty($title)) {
-        $error = "Veuillez renseigner un titre.";
+    $update_link_requete = $db_connect->prepare("UPDATE link
+                                                 SET url_link = ?, titre_link = ?, description_link = ?
+                                                 WHERE id_link = ?");
+    $execution_request=$update_link_requete->execute([$url, $title, $description, $id_link]);
+
+    if ($execution_request) {
+        $execution_request_message = "<p>Link modifié avec succès !</p>";
     } else {
-        $create_link_request = $db_connect->prepare("INSERT INTO link(url_link, titre_link, description_link, date_link)
-                                                     VALUES(?,?,?, NOW())");
-        $execution_request=$create_link_request->execute([$url, $title, $comment]); 
-    
-        if ($execution_request) {
-            echo "Link ajouté avec succès !";
-        } else {
-            echo "Erreur lors de l'ajout du link ...";
-        }
+        $execution_request_message = "<p>Erreur lors de la modification du link ...</p>";
     }
+    
+    // Recharger les données du lien après la modification,
+    $title_link_requete = $db_connect->prepare("SELECT * 
+                                                FROM link 
+                                                WHERE id_link = ?");
+    $title_link_requete->execute([$id_link]);
+    $affichage_titre_lien = $title_link_requete->fetch(PDO::FETCH_ASSOC);
+
+    // pour garder les valeurs une fois le formulaire validé
+    $url = htmlspecialchars($url);
+    $title = htmlspecialchars($title);
+    $description = htmlspecialchars($description);
+} elseif ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET["id_link"])) {
+    // en cas de soucis on garde de coté les valeurs dans $affichage_titre_lien
+    $url=htmlspecialchars($affichage_titre_lien['url_link']);
+    $title=htmlspecialchars($affichage_titre_lien['titre_link']);
+    $description=htmlspecialchars($affichage_titre_lien['description_link']);
+} else {
+    // si aucun link n'est trouvé
+    $url='';
+    $title='';
+    $description='';
 }
 ?>
 
@@ -56,23 +90,34 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         </nav>
 
         <div>
-            <h2>Modifier le link : </h2>
+            <h2>
+                <?php echo isset($affichage_titre_lien['titre_link']) ? ("Modifier le link :").htmlspecialchars($affichage_titre_lien['titre_link']) : ''; ?>
+            </h2>
 
             <form method="POST">
+                <input type="hidden" name="id_link" value="<?php echo $affichage_titre_lien['id_link']; ?>">
+
                 <div>
-                    <label for="url">URL (*) :</label>
-                    <input type="url" name="url" id="url_link">
+                    <label for="url">URL :</label>
+                    <input type="url" name="url" value="<?php echo $url; ?>" id="url_link">
                 </div>
                 <div>
-                    <label for="title">Link (*) :</label>
-                    <input type="text" name="title" id="titre_link">
+                    <label for="title">Link :</label>
+                    <input type="text" name="title" value="<?php echo $title; ?>" id="titre_link">
                 </div>
                 <div>
-                    <label for="comment">Commentaire :</label>
-                    <input type="text" name="comment" id="description_link">
+                    <label for="description">description :</label>
+                    <input type="text" name="description" value="<?php echo $description; ?>" id="description_link">
                 </div>
-                <button type="submit">PUBLIER</button>
+                <button type="submit">Modifier</button>
             </form>
+
+            <?php if ($execution_request_message) { ?>
+            <div>
+                <p><?php echo $execution_request_message ?></p>
+            </div>
+            <?php }; ?>
+
         </div>
     </main>
 
