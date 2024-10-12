a7c6d2465599a3824571fe3019c6d5084d9e7946
Madeline Ricateau <madeline.ricateau@outlook.fr>
2024-10-12 19:17:32 +0200

    Ajout requêtes SQL de mise à jour de données

---
+++ post_code/requetes_sql/CRUD/#tests/UPDATE_sql/miseAJourDonnee.sql
@@ -0,0 +1,3 @@
+UPDATE link
+SET description_link = "Did you know that you can layer CSS gradients on top of each other? </br>I first started playing with the concept when checking out this tool for making mesh gradients."
+WHERE id_link = 1;
---
+++ post_code/requetes_sql/CRUD/UPDATE_sql/miseAJourDonnee.sql
@@ -0,0 +1,3 @@
+UPDATE link
+SET url_link = ?, titre_link = ?, description_link = ?
+WHERE id_link = ?
